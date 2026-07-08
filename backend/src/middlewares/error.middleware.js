5f6a8766338f4777f5bdc305d464d4820878c635
coronelhernan <hcoronel1996@gmail.com>
2026-07-08 11:30:32 -0300

    feat: agregar middleware de manejo de errores con soporte para codigos de Prisma"

---
+++ backend/src/middlewares/error.middleware.js
@@ -0,0 +1,24 @@
+const { error } = require("../utils/response");
+
+function errorHandler(err, req, res,) {
+	console.error(err);
+
+	// Validando errores de Prisma para conflictos
+	if (err.code === "P2002") {
+		// Violacion de constraint unique
+		const field = err.meta?.target?.join(", ") || "campo";
+    return error(res, `Ya existe un registro con ese ${field}`, 409);
+	}
+
+	// Validando errores de Prisma para recursos no encontrados (404 not found)
+	if (err.code === "P2025") {
+		return error(res, "Recurso no encontrado", 404);
+	}
+
+	// Manejo de errores genéricos
+	const status = err.status || 500;
+	const message = err.message || "Error interno del servidor";
+	return error(res, message, status);
+};
+
+module.exports = errorHandler;
