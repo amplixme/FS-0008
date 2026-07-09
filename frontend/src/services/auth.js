3bf1cebfbab08edbcfc36aaafdee08951be6feec
redmor1 <lightz17@hotmail.com>
2026-07-09 18:18:57 -0300

    fix: Arreglar acceso al response de API en login

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,5 +1,6 @@
 import api from "./api";
 
 export async function loginUser({ email, password }) {
-  return api.post("/auth/login", { email, password });
+  const response = await api.post("/auth/login", { email, password });
+  return response.data;
 }
