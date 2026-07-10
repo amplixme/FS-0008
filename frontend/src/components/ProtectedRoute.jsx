5f841ba2e67bba46f9d2990d018479568d14cfab
redmor1 <lightz17@hotmail.com>
2026-07-10 11:41:08 -0300

    feat: Implementar AuthContext y rutas protegidas

---
+++ frontend/src/components/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import useAuth from "../hooks/useAuth";
+import { Navigate } from "react-router";
+
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useAuth();
+
+  if (!isAuthenticated) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+}
+
+export default ProtectedRoute;
