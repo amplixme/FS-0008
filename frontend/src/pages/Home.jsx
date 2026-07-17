0df102f8e26166526429a68068456f8d4638fc5d
coronelhernan <hcoronel1996@gmail.com>
2026-07-17 14:31:30 -0300

    refactor: refactorizar paginas Home, EditPost y PostDetail para que utilicen los componentes de Spinner, errorMessage y EmptyState

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,12 @@
 import PostCard from "../components/posts/PostCard";
-import { POSTS } from "../data/data";
 import { useEffect, useState } from "react";
 import Sidebar from "../components/layout/Sidebar";
 import Pagination from "../components/ui/Pagination";
-import LoadingSpinner from "../components/ui/LoadingSpinner";
-import ErrorState from "../components/ui/ErrorState";
-import EmptyState from "../components/ui/EmptyState";
 import HeroSearch from "../components/home/HeroSearch";
 import { getAll } from "../services/post.service";
+import Spinner from "../components/common/Spinner";
+import ErrorMessage from "../components/common/ErrorMessage";
+import EmptyState from "../components/common/EmptyState";
 
 function Home() {
   const [posts, setPosts] = useState([]);
@@ -48,12 +47,12 @@ function Home() {
 
         <div className="flex-1">
           {isLoading ? (
-            <LoadingSpinner
+            <Spinner
               icon="progress_activity"
               message="Cargando publicaciones..."
             />
           ) : error ? (
-            <ErrorState
+            <ErrorMessage
               icon="error"
               message="Ha ocurrido un error al cargar los datos"
               onRetry={handleRetry}
