3c6d320c5b36c3415166bd3d0c542b2338c3d41a
coronelhernan <hcoronel1996@gmail.com>
2026-07-21 17:14:09 -0300

    feat: agregar middleware para subir archivos

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,5 +1,6 @@
 import { v2 as cloudinary } from "cloudinary";
 
+// En esta configuración forzamos "secure" para que las URLs devueltas sean siempre https.
 cloudinary.config({
   secure: true,
 });
