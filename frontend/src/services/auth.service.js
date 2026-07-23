8de1977961cd3f30fe08f2e4d98a804fc784c34e
coronelhernan <hcoronel1996@gmail.com>
2026-07-23 16:58:06 -0300

    refactor: agregar validaciones para subir imagenes en el componente ImageUpload.jsx

--- frontend/src/services/auth.service.js
+++ frontend/src/services/auth.service.js
@@ -4,9 +4,8 @@ export async function registerUser({ name, email, password }) {
   const response = await api.post("/auth/register", { name, email, password });
   return response;
 }
+
 export async function loginUser({ email, password }) {
   const response = await api.post("/auth/login", { email, password });
   return response;
 }
-
-
