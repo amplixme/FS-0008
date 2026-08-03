70a3779f33d5a1d14348089f22e70709a113f91d
coronelhernan <hcoronel1996@gmail.com>
2026-08-03 10:45:43 -0300

    feat: agregar funciones obtener perfil y actualizar perfil en user.service.js

--- backend/src/services/user.service.js
+++ backend/src/services/user.service.js
@@ -1,1 +1,55 @@
-// Services
+import prisma from "../config/prisma.js";
+
+export const getPublicProfile = async (id) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: Number(id),
+    },
+    select: {
+      id: true,
+      name: true,
+      bio: true,
+      avatarUrl: true,
+      _count: {
+        select: {
+          posts: true,
+        },
+      },
+    },
+  });
+
+  if (!user) {
+    const error = new Error("Usuario no encontrado");
+    error.status = 404;
+    throw error;
+  }
+
+  return {
+    id: user.id,
+    name: user.name,
+    bio: user.bio,
+    avatarUrl: user.avatarUrl,
+    postsCount: user._count.posts,
+  };
+};
+
+export const updateProfile = async (userId, data) => {
+  const { name, bio, avatarUrl } = data;
+
+  return await prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      name,
+      bio,
+      avatarUrl,
+    },
+    select: {
+      id: true,
+      name: true,
+      bio: true,
+      avatarUrl: true,
+    },
+  });
+};
