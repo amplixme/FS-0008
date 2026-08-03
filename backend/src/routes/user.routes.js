a37b0559051a258af645a3a068d7097f61835554
coronelhernan <hcoronel1996@gmail.com>
2026-08-03 11:08:25 -0300

    feat: agregar rutas para user

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,1 +1,17 @@
-// Routes
+import { Router } from "express";
+import * as userController from "../controllers/user.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+
+const router = Router();
+
+// GET /api/users/:id - Ruta para mostrar el perfil Público
+router.get("/:id", userController.getPublicProfile);
+
+// PUT /api/users/me - Ruta para actualizar el perfil
+router.put(
+  "/me",
+  authMiddleware,
+  userController.updateProfile
+);
+
+export default router;
