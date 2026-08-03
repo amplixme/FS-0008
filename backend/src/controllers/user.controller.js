784c98ca239fc538aee5a6f11d53fb95794d779d
coronelhernan <hcoronel1996@gmail.com>
2026-08-03 11:04:03 -0300

    feat: agregar controladores para obtener perfil y actualizar en user.controller.js

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,1 +1,31 @@
-// Controllers
+import userService from "../services/user.service.js";
+import { success } from "../utils/response.js";
+
+export const getProfile = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const profile = await userService.getPublicProfile(id);
+
+    return success(res, profile, 200);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const update = async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const { name, bio, avatarUrl } = req.body;
+
+    const updatedProfile = await userService.updateProfile({
+      userId,
+      name,
+      bio,
+      avatarUrl,
+    });
+
+    return success(res, updatedProfile, 200);
+  } catch (error) {
+    next(error);
+  }
+};
