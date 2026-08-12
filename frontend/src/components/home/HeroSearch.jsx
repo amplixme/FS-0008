7219b1c69c83e16a7f525ff2a57a51490c5e0b09
coronelhernan <hcoronel1996@gmail.com>
2026-08-12 12:15:52 -0300

    fix: corregir título últimas publicaciones pagina principal para vista mobile

--- frontend/src/components/home/HeroSearch.jsx
+++ frontend/src/components/home/HeroSearch.jsx
@@ -49,9 +49,9 @@ function HeroSearch() {
 
   return (
     <section className="mb-16">
-      <div className="relative p-12 rounded-3xl overflow-hidden bg-linear-to-br from-primary/5 to-primary-container/10">
+      <div className="relative lg:p-12 p-6 rounded-3xl overflow-hidden bg-linear-to-br from-primary/5 to-primary-container/10">
         <div className="relative z-10 max-w-2xl">
-          <h1 className="text-5xl font-extrabold text-on-surface mb-6 tight-tracking leading-tight">
+          <h1 className="text-3xl lg:text-5xl font-extrabold text-on-surface mb-6 tight-tracking leading-tight">
             Últimas publicaciones
           </h1>
           <div className="relative flex items-center">
